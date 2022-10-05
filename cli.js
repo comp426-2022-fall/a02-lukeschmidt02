88ae93fa24e0bb011e9d7d129c946294cc972cf1
lukeschmidt02 <77812790+lukeschmidt02@users.noreply.github.com>
2022-10-04 22:09:20 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -9,6 +9,7 @@ const args = minimist(process.argv.slice(2));
 let timezone = moment.tz.guess();
 let latitude = '0';
 let longitude = '0';
+let timeline = 'tomorrow';
 
 if (args.h) {
     console.log(`
@@ -37,12 +38,12 @@ if (args.w) {
 if (args.t) {
     timezone = args.t;
 }
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=' + timezone)
 const data = await response.json();
 
 if (args.j) {
     try {
-        console.log(data);
+        console.log(data); 
     } catch(err) {
         process.exitCode = 1;
         console.log('exitcode 1');
@@ -54,14 +55,22 @@ if (args.j) {
 }
 
 if (args.d !== undefined) {
-    console.log(data)
     const days = args.d
-    
+
     if (days == 0) {
-        console.log("today.")
+        timeline = "today"
     } else if (days > 1) {
-        console.log("in " + days + " days.")
-    } else {
-        console.log("tomorrow.")
+        timeline = "in " + days + " days"
     }
+}
+
+if ((args.n !== undefined || args.s !== undefined) && (args.e !== undefined || args.w !== undefined)) {
+    let days = 1; 
+    if (args.d !== undefined) {
+        days = args.d;
+    }
+    
+    console.log("In the timezone " + timezone + ", the weather " + timeline + " at latitude: " + latitude + " and longitude: " +
+    longitude + " is as follows.\nThe temperature high is " + data.daily.temperature_2m_max[days] + " and low is " + data.daily.temperature_2m_min[days] + " degrees fahrenheit. The max windspeed is " +
+    data.daily.windspeed_10m_max[days] + " mph. There are " + data.daily.precipitation_hours[days] + " hours of rain.\nSunrise is at " + data.daily.sunrise[days].substr(data.daily.sunrise[days].length - 5) + " and sunset is at " + data.daily.sunset[days].substr(data.daily.sunset[days].length - 5) + " given the previously noted timezone.")
 }
